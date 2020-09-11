6d0df19c9d43bb310d9f90a4188de9afbe9a5fcc
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-11 22:37:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,7 @@ class Scraper
         student[:blog] = link
       end
     end
-    vitals = profile_page.css("vitals-text-container")
+    vitals = profile_page.css("vitals-text-container").children.css
     student
   end
 
