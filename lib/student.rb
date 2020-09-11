ae710c349a74d5c13ae3fb01929183bb8191bf30
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-11 22:54:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
     @@all << self
   end
 
