fc102e542e3b5a64497fd85b8a0f55cbdb1dbae2
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-11 22:36:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,33 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css("div.student-card").each do |student|
+        student_name.css(".student-name").text
+        student_location.css(".student-location").text
+        student_profile_url = "#{student.attr('href')}"
+        students << {name: student_name, location: student_location, profile_url: student_profile_url}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    index_page = Nokogiri::HTML(open(index_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map {|el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
   end
 
 end
