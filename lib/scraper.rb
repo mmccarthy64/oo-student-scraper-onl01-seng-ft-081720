e9e009cfea45469eba7585d63d6f5481aeaa0f28
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-11 23:04:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student = {}
-    index_page = Nokogiri::HTML(open(profile_url))
+    profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").children.css("a").map {|el| el.attribute('href').value}
     links.each do |link|
       if link.include?("linkedin")
